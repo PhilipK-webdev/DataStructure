e23b0957a85f113bcd8a91b368404b9ac206ae04
PhilipK-webdev <philipk179@gmail.com>
2020-11-14 16:12:36 -0800

    working on the linked list

--- src/Pages/LinkedList/LinkedListData.js
+++ src/Pages/LinkedList/LinkedListData.js
@@ -1,10 +1,31 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "../LinkedList/LinkedList.css"
 const LinkedListData = () => {
+
+    const ShapeLinkedList = () => {
+        return (
+            <div className="body">
+                <div className="shape" style={{ display: "flex", flexDirection: "row !important" }}>
+                    <p>data1</p>
+                    <strong><hr /></strong>
+                    <p>data2</p>
+                </div>
+                <div id="pointer" style={{ marginRight: "20px" }} ></div>
+            </div>
+        )
+    }
+
     return (
 
         <div>
-            <h1>Helloo from LinkedList</h1>
+            <div className="container">
+                <label className="form__label" forhtml="add">Add to linked list : </label>
+                <input type="text" id="inputPush" name="add" placeholder="add..." className="form__input" />
+                <button type="submit" className="button4" style={{ marginTop: "10px" }}>Add</button>
+            </div>
+            <div className="containerLinked">
+                <ShapeLinkedList />
+            </div>
         </div>
 
     )

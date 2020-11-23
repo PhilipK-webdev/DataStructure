67decf8a5987def789dfefbfdddd309d69931305
PhilipK-webdev <philipk179@gmail.com>
2020-11-22 18:54:34 -0800

    add animation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,14 @@ const Home = () => {
             <h1>Welcome</h1>
             <div className="intro">
                 <h3>Introduction</h3>
-                <p> Abstrat Data Structure more info you can find here <a href="https://computersciencewiki.org/index.php/Abstract_data_structures">Link</a><br />
+                <p>
+                    One of the first things as a student that you learn is Data Structure .<br />
+                    There is many Data Sturcture in computer science.<br />
+                    Here you can find 4 of this data structure. What are their names ,their functionality and their visualization.<br />
+                    More info regarding what is a data structure you can find here at wikipedia <a href="https://computersciencewiki.org/index.php/Abstract_data_structures" target="_blank">Link.</a><br />
                 When you click on one of the data structure <br />
-                Then you can learn and play with the functionality of each data structure you choose.
-                Enjoy!
+                Then you can learn and play with the functionality .<br />
+                Enjoy 😎!
                 </p>
             </div>
         </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,22 +7,22 @@ const Nav = () => {
             <header>
                 <nav>
                     <div className="home">
-                        <Link to="/" id="link">
+                        <Link to="/" id="link" className="link">
                             Home
                         </Link>
                     </div>
                     <ul>
                         <li>
-                            <Link to="/stack">Stack</Link>
+                            <Link to="/stack" className="link">Stack</Link>
                         </li>
                         <li>
-                            <Link to="/linkedList">Linked List</Link>
+                            <Link to="/linkedList" className="link">Linked List</Link>
                         </li>
                         <li>
-                            <Link to="/array">Array</Link>
+                            <Link to="/array" className="link">Array</Link>
                         </li>
                         <li>
-                            <Link to="/queue">Queue</Link>
+                            <Link to="/queue" className="link">Queue</Link>
                         </li>
                     </ul>
                 </nav>
